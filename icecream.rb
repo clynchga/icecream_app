94f80a97bc90d762deee5d371b524caefe6cc9b2
clynchga <clarevlynch@gmail.com>
2014-09-25 13:11:57 -0400

    add icecream.rb explaining my favorite flavor of ice cream

---
+++ icecream.rb
@@ -0,0 +1,5 @@
+# my favorite flavor of ice cream is Razzoreo
+# what, you may ask, is razzoreo?
+# it is the beautiful combination of rasperry icecream and crushed oreos
+# it is like cookies & cream ice cream on steroids
+# and that is why it is my favorite
